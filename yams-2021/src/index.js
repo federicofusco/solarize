4276197140bb64e9da8697950d878edeae78ad00
federicofusco <federico.fusco@overslight.com>
2021-10-02 10:42:37 +0000

    Created empty React project

---
+++ yams-2021/src/index.js
@@ -0,0 +1,12 @@
+// Imports React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Imports CSS
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
