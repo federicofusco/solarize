934fa89a071c45f5411b90ae3634995d047d0a1d
federicofusco <federico.fusco@overslight.com>
2021-10-02 16:39:12 +0000

    Wrote a very poorly optimized function to calcuate the average Solar Irradiance of each week

--- yams-2021/src/pages/App.js
+++ yams-2021/src/pages/App.js
@@ -27,9 +27,49 @@ class App extends React.Component {
     componentDidMount () {
 
         ApiHandler.FetchAllSkySurfaceShortwaveDownwardIrradiance ()
-        .then ( ( result ) => {
-            console.log(result.data);
-        })
+        .then ( async ( result ) => {
+
+            var rawData = result.data.data[9];
+            var temp = [];
+            var values = [];
+
+            // Loops through the raw data and removes all the dates
+            // Then valculates the average for each week
+            for ( var x = 6; x < rawData.length; x++ ) {
+
+                if ( rawData[x].includes ( "\n" ) ) {
+
+                    if ( temp.length < 7 ) {
+                        
+                        temp.push ( Number ( rawData[x].split ( "\n" )[0] ) );
+        
+                    } else {
+
+
+                        // Calculates the average value of each day
+                        var average = 0;
+                        for ( var y = 0; y < temp.length; y++ ) {
+
+                            average += temp[y];
+
+                        }
+
+                        average /= temp.length + 1;
+
+                        values.push ( average );
+
+                        temp = [];
+                        temp.push ( Number ( rawData[x].split ( "\n" )[0] ) );
+
+                    }
+
+                }
+
+            }
+
+            console.log( values );
+
+        });
 
     }
 
