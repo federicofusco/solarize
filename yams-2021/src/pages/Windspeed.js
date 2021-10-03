cbb1791cb188f3cc9cec6c37e314604502847c8c
federicofusco <federico.fusco@overslight.com>
2021-10-03 20:37:50 +0000

    Added description to windspeed page

--- yams-2021/src/pages/Windspeed.js
+++ yams-2021/src/pages/Windspeed.js
@@ -5,6 +5,7 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "./../components/Navbar";
 import Chart from "../components/Chart";
+import Description from "../components/Description";
 
 // Imports utils
 import ApiHandler from "../utils/ApiHandler";
@@ -166,7 +167,18 @@ class Windspeed extends React.Component {
                     Parameter="WS10M" />
                 }
 
-            </div>
+		<Description
+		Name="Windspeed"
+		Description="In meteorology, wind speed, or wind flow speed, is a fundamental atmospheric quantity caused by air moving from high to low pressure, usually due to changes in temperature. Wind speed is now commonly measured with an anemometer."
+		Sources="https://en.wikipedia.org/wiki/Wind_speed"
+		Uses={[
+		    {
+			title: "Construction",
+			content: "Wind speed is a common factor in the design of structures and buildings around the world. It is often the governing factor in the required lateral strength of a structure's design."	
+		    }
+		]} />
+            
+	    </div>
         )
 
     }
