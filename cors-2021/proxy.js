7e37ca0beaa22dc00da6de2ef3c2b85078c4d7da
federicofusco <federico.fusco@overslight.com>
2021-10-03 13:41:39 +0000

    Fixed proxy CORS origin header

--- cors-2021/proxy.js
+++ cors-2021/proxy.js
@@ -12,7 +12,7 @@ app.use ( ( request, response, next) => {
 
     // Sets up CORS Proxy
     response.header('Access-Control-Allow-Credentials', 'true');
-    response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept, Set-Cookie, *');
     next ();
