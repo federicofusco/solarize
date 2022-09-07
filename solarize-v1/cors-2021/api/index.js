daa993528a2117ad63a6d6be9d497a1cc68937c6
federicofusco <federico.fusco@overslight.com>
2022-09-07 19:01:00 +0200

    Setting up CORS proxy with vercel since the Heroku instance will go offline in October

--- solarize-v1/cors-2021/api/index.js
+++ solarize-v1/cors-2021/api/index.js
@@ -8,13 +8,14 @@ const Axios = require ( "axios" ).default;
 
 // Express middleware
 app.use ( express.json () );
-app.use ( ( request, response, next) => {
+app.use ( ( _, response, next ) => {
 
     // Sets up CORS Proxy
     response.header('Access-Control-Allow-Credentials', 'true');
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept, Set-Cookie, *');
+	response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     next ();
 
 });
@@ -23,27 +24,18 @@ app.get ( "/proxy/:url", async ( request, response ) => {
 
     // Checks that the URL query param has been passed
     if ( !request.params.url ) {
-
         response.sendStatus ( 400 );
-
     }
 
     // Fetches data
     Axios.get ( request.params.url )
     .then ( ( result ) => {
-
         response.status ( 200 ).json ( result.data );
-
     })
     .catch ( ( error ) => {
-
         response.status ( error.response.status || 500 ).json ( error.response.data );
+    });
 
-    })
-
-})
-
-// Listens for requests
-app.listen ( process.env.PORT || 5000, () => {
-    console.log ( "[PROXY/STATUS]: Proxy online!" );
 });
+
+module.exports = app;
