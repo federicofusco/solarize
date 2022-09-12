f410c645bf089dcd2be132fbb933c5593d9118d1
federicofusco <federico.fusco@overslight.com>
2022-09-12 21:52:30 +0200

    Added rudamentary notification system

--- 2021/solarize-v2/hooks/logging.js
+++ 2021/solarize-v2/hooks/logging.js
@@ -1,42 +1,62 @@
+import { useSnackbar } from "notistack";
+
 /**
  * A hook to render logging and debugging easier and more streamline
  * @version 0.0.1
  */
 const useLogging = () => {
 
+	const { enqueueSnackbar } = useSnackbar ();
+
 	/**
      * Sends an success message to the console
      * 
-     * @param {object} data An object containing the data which should be logged
-     * @param {function} callback The function which should execute when the data has been logged
+     * @param data - An object containing the data which should be logged
+     * @param callback - The function which should execute when the data has been logged
+	 * @param displayNotification - Whether or not to display the notification to the user 
      * @example 
      * logInfo ({
      *   code: "auth/user-authenticated",
      *   message: "User friendly success message"
      * }, resolve);
      */
-	const logInfo = ( data, callback ) => {
+	const logInfo = ( data, callback, displayNotification = true ) => {
 		console.info ( `[${ data.code }]: ${ data.message }` );
+
 		if ( callback ) {
 			callback ( data );
+		}
+
+		if ( displayNotification ) {
+			enqueueSnackbar ( data.message, {
+				variant: "info"
+			});
 		}
 	}
 
 	/**
      * Sends an error message to the console
      * 
-     * @param {object} data An object containing the data which should be logged
-     * @param {function} callback The function which should execute when the data has been logged
+     * @param data - An object containing the data which should be logged
+     * @param callback - The function which should execute when the data has been logged
+	 * @param displayNotification - Whether or not to display the notification to the user 
      * @example 
      * logError ({
      *   code: "auth/user-unauthenticated",
      *   message: "User friendly error message"
      * }, reject);
      */
-	 const logError = ( data, callback ) => {
+	 const logError = ( data, callback, displayNotification = true ) => {
         console.warn ( `[${ data.code }]: ${ data.message }` );
+
         if ( callback ) {
 			callback ( data );
+		}
+
+		if ( displayNotification ) {
+			enqueueSnackbar ( data.message, {
+				variant: "error"
+			});
 		}
     }
 
--- 2021/solarize-v2/pages/_app.js
+++ 2021/solarize-v2/pages/_app.js
@@ -1,7 +1,15 @@
+// Imports styles
 import '../styles/globals.css'
 
+// Imports notification provider
+import { SnackbarProvider } from "notistack";
+
 const SolarizeApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />
+	return ( 
+		<SnackbarProvider autoHideDuration={ 1500 } maxSnack={ 3 } preventDuplicate>
+			<Component {...pageProps} />
+		</SnackbarProvider>
+	)
 }
 
 export default SolarizeApp;
