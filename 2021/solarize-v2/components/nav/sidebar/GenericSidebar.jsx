25620411fe19ccfa7f848b823dd6fd3b5ca4a1cd
federicofusco <federico.fusco@overslight.com>
2022-09-12 21:29:57 +0200

    Fixed issue #9

--- 2021/solarize-v2/components/nav/sidebar/GenericSidebar.jsx
+++ 2021/solarize-v2/components/nav/sidebar/GenericSidebar.jsx
@@ -12,7 +12,7 @@ const GenericSidebar = ({ visible, toggleSidebar, title, children }) => {
 	return (
 		<div className={`overflow-auto transition-all duration-150 z-50 w-screen sm:w-1/2 md:w-2/5 lg:w-1/3 h-screen border-r border-gray-300 fixed transform top-1/2 left-0 ${ visible ? "translate-x-0 -translate-y-1/2" : "-translate-x-full -translate-y-1/2" } bg-white px-8 pb-6`}>
 				
-				<div className="w-full h-20 flex justify-between">
+				<div className="w-full h-20 mt-8 sm:mt-0 flex justify-between">
 
 					{/* Title */}
 					<h1 className="font-poppins font-bold text-4xl my-auto">{ title }</h1>
