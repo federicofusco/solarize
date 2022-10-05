423b7dbbb53162bcb92623764d59c2d574483a11
Federico Fusco <federicofusco0@gmail.com>
2022-10-05 14:59:25 +0200

    Fixed repo structure

--- 2022/v1/components/nav/Logo.jsx
+++
@@ -1,5 +0,0 @@
-const Logo = () => {
-    return <p className="text-white m-auto font-poppins font-bold text-4xl">SeisMoon</p>
-}
-
-export default Logo;
--- 2022/v1/components/nav/SidebarIcon.jsx
+++
@@ -1,5 +0,0 @@
-const SidebarIcon = ({ onClick }) => {
-    return <i aria-hidden onClick={ onClick } className="my-auto text-3xl text-white fas fa-bars"></i>;
-}
-
-export default SidebarIcon;
